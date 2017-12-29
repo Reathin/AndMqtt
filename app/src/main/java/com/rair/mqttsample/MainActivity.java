b8c496a3d76f46fa11aef3edac9fe7faa3e5acc4
Rair <645207307@qq.com>
2017-12-29 16:12:00 +0800

    update

--- app/src/main/java/com/rair/mqttsample/MainActivity.java
+++ app/src/main/java/com/rair/mqttsample/MainActivity.java
@@ -6,6 +6,9 @@ import android.util.Log;
 
 import com.rairmmd.andmqtt.AndMqtt;
 import com.rairmmd.andmqtt.MqttConnect;
+import com.rairmmd.andmqtt.MqttPublish;
+import com.rairmmd.andmqtt.MqttSubscribe;
+import com.rairmmd.andmqtt.MqttUnSubscribe;
 
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
@@ -42,10 +45,11 @@ public class MainActivity extends AppCompatActivity {
             }
         }).connect(new MqttConnect().setClientId("android")
                 .setPort(1884)
-                .setServer("tcp://47.100.100.235"), new IMqttActionListener() {
+                .setServer("tcp://xx.xx.xx.xx"), new IMqttActionListener() {
             @Override
             public void onSuccess(IMqttToken asyncActionToken) {
                 Log.i("Rair", "(MainActivity.java:51)-onSuccess:->连接成功");
+                subscribe();
             }
 
             @Override
@@ -53,51 +57,53 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("Rair", "(MainActivity.java:56)-onFailure:->连接失败");
             }
         });
+    }
 
-//        //订阅
-//        AndMqtt.getInstance().subscribe(new MqttSubscribe()
-//                .setTopic("主题")
-//                .setQos(0), new IMqttActionListener() {
-//            @Override
-//            public void onSuccess(IMqttToken asyncActionToken) {
-//                Log.i("Rair", "(MainActivity.java:63)-onSuccess:->订阅成功");
-//            }
-//
-//            @Override
-//            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-//                Log.i("Rair", "(MainActivity.java:68)-onFailure:->订阅失败");
-//            }
-//        });
-//
-//        //发布
-//        AndMqtt.getInstance().publish(new MqttPublish()
-//                .setMsg("消息")
-//                .setQos(0)
-//                .setTopic("主题"), new IMqttActionListener() {
-//            @Override
-//            public void onSuccess(IMqttToken asyncActionToken) {
-//                Log.i("Rair", "(MainActivity.java:79)-onSuccess:->发布成功");
-//            }
-//
-//            @Override
-//            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-//                Log.i("Rair", "(MainActivity.java:84)-onFailure:->发布失败");
-//            }
-//        });
-//
-//        //取消订阅
-//        AndMqtt.getInstance().unSubscribe(new MqttUnSubscribe()
-//                .setTopic("主题"), new IMqttActionListener() {
-//            @Override
-//            public void onSuccess(IMqttToken asyncActionToken) {
-//                Log.i("Rair", "(MainActivity.java:93)-onSuccess:->取消订阅成功");
-//            }
-//
-//            @Override
-//            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-//                Log.i("Rair", "(MainActivity.java:98)-onFailure:->取消订阅失败");
-//            }
-//        });
+    private void subscribe() {
+        //订阅
+        AndMqtt.getInstance().subscribe(new MqttSubscribe()
+                .setTopic("主题")
+                .setQos(0), new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken asyncActionToken) {
+                Log.i("Rair", "(MainActivity.java:63)-onSuccess:->订阅成功");
+            }
+
+            @Override
+            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                Log.i("Rair", "(MainActivity.java:68)-onFailure:->订阅失败");
+            }
+        });
+
+        //发布
+        AndMqtt.getInstance().publish(new MqttPublish()
+                .setMsg("消息")
+                .setQos(0)
+                .setTopic("主题"), new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken asyncActionToken) {
+                Log.i("Rair", "(MainActivity.java:79)-onSuccess:->发布成功");
+            }
+
+            @Override
+            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                Log.i("Rair", "(MainActivity.java:84)-onFailure:->发布失败");
+            }
+        });
+
+        //取消订阅
+        AndMqtt.getInstance().unSubscribe(new MqttUnSubscribe()
+                .setTopic("主题"), new IMqttActionListener() {
+            @Override
+            public void onSuccess(IMqttToken asyncActionToken) {
+                Log.i("Rair", "(MainActivity.java:93)-onSuccess:->取消订阅成功");
+            }
+
+            @Override
+            public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                Log.i("Rair", "(MainActivity.java:98)-onFailure:->取消订阅失败");
+            }
+        });
     }
 
     @Override
